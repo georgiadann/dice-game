1e8f3dbfdb2f335c720bebb7f02a09c4b81843b2
Georgia Dann <78177141+georgiadann@users.noreply.github.com>
2021-08-01 22:57:05 +0000

    making user be able to choose to play race or round game

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 //dice game program - version 1
 //Georgia Dann
 //27/07/2021
-// let gameChoice = prompt('would you like to play a with rounds or a race to a certain number?');
-let gameChoice = prompt('would you like to play the game as round or race?');
-while(gameChoice <= 0 || isNaN(gameChoice) || gameChoice == null || gameChoice % 1 != 0){
-  console.log('Not a valid entry')
-  let gameChoice = prompt('would you like to play the game as round or race?');
-  if(gameChoice == race){
-    function race()
+
+
+let pick = 0;
+let gameChoice = null;
+while(!isNaN(gameChoice) || gameChoice == null){
+    gameChoice = prompt('would you like to play the game as round or race?');
+  if(gameChoice == 'race'){
+    let pick = raceChoice();
+  } else if(gameChoice == 'round'){
+    let pick = roundChoice();
+  }else{
+    console.log('Not a valid entry')
   }
+  
 }
-if(gameChoice == round){
+
+function raceChoice(){
+  let racePick = prompt('What point number would you like to race to?');
+  while(racePick <= 0 || isNaN(racePick) || racePick == null || racePick % 1 != 0){
+  console.log('Not a valid number');
+   racePick = prompt('What point number would you like to race to?');
+  } 
+  return racePick
+}
+
+ function roundChoice(){
   let roundPick = prompt('How many rounds would you like to play?');
   while(roundPick <= 0 || isNaN(roundPick) || roundPick == null || roundPick % 1 != 0){
   console.log('Not a valid number');
   roundPick = prompt('How many rounds would you like to play?');
-}} else if (gameChoice == race){
-  let racePick = prompt('What point number would you like to race to?');
-  while(racePick <= 0 || isNaN(racePick) || racePick == null || racePick % 1 != 0){
-  console.log('Not a valid number');
-  let racePick = prompt('What point number would you like to race to?');
-} 
+  }
+  return roundPick;
+}
 
-
-// let roundPick = prompt('How many rounds would you like to play? 1-10');
-
-// while(roundPick <= 0 || isNaN(roundPick) || roundPick == null || roundPick % 1 != 0){
-//   console.log('Not a valid number')
-//   roundPick = prompt('How many rounds would you like to play? 1-10');
-  
-// }
-
+function getNames(){
 // having list of player names ready to be picked
 randomName1 = ['pogchamp42069','swaggyolympian','spicyfeet','Mark',];
 let newRandomName1 = randomName1[Math.floor(Math.random()* 4 + 0 )];
@@ -51,21 +56,40 @@ if(player2 == ''){
 } else{
   console.log('Welcome ' + player2 + ', good luck!');
 }
-
-//setting round and points to 0
 let name1 = (player1 || newRandomName1);
 let name2 = (player2 || newRandomName2);
+return {name1: name1, name2: name2};
+}
+
+//setting round and points to 0
+
 let pointPlayer1 = 0;
 let pointPlayer2 = 0;
 let round = 1;
-//if rund is smaller than the amount of rounds picked it will play and repeat game 
-while(round<=roundPick){
-  play()
+let names = getNames();
+
+ 
+
+
+//if round is smaller than the amount of rounds picked it will play and repeat game 
+
+if(gameChoice == 'race'){
+  playRace(names);
+} else if(gameChoice == 'round'){
+  while(round<= pick){
+    playRound(names);
+    }
 }
-function play(){
+
+
+
+//if player picks race it will play this function
+function playRace(names){  
   roll1 = Math.floor(Math.random() * 6 + 1);
   roll2 = Math.floor(Math.random() * 6 + 1);
-do{
+  let name1 = names.name1
+  let name2 = names.name2
+
   //loop for player 1 to roll the dice
  let player1Roll = prompt(name1 + ' type "roll" to roll the dice');
  while(player1Roll != 'roll'){
@@ -89,13 +113,70 @@ if(roll1 > roll2){
   console.log('Its a draw!');
 }
 
-// if (roll1==2){
-//   console.log`${name1} gets 2 points!`;
-//   pointPlayer1+2;
-// } else if (roll2==2){
-//   console.log`${name2} gets 2 points!`;
-//   pointPlayer2+2;
-// }
+//make a winner for after rounds
+let winner = '';
+if(pointPlayer1 > pointPlayer2){
+   winner = name1;
+}else if(pointPlayer1 < pointPlayer2){
+   winner = name2;
+} else{
+   winner = `${name1} ${name2}`;
+}
+
+//state winner of the round
+ if(pointPlayer1 >= racePick){
+    console.log('Congratulations '+ name1 + ' You won!');
+    // return;
+  } else if(pointPlayer2>= racePick){
+    console.log('Congratulations '+ name2 + ' You won!');
+    // return;
+  }else if(pointPlayer1< racePick){
+    
+let plural = pointPlayer1!=1?'s':'';
+console.log(`${name1} has ${pointPlayer1} point${plural}!`)
+let plural2 = pointPlayer2!=1?'s':'';
+console.log(`${name2} has ${pointPlayer2} point${plural2}!`)
+    round++
+
+  } else if(pointPlayer2 < racePick){
+    let plural = pointPlayer1!=1?'s':'';
+console.log(`${name1} has ${pointPlayer1} point${plural}!`)
+let plural2 = pointPlayer2!=1?'s':'';
+console.log(`${name2} has ${pointPlayer2} point${plural2}!`)  
+  round++
+  }
+}
+
+
+//if player picks round game it will play this function
+function playRound(names){  
+  roll1 = Math.floor(Math.random() * 6 + 1);
+  roll2 = Math.floor(Math.random() * 6 + 1);
+  let name1 = names.name1
+  let name2 = names.name2
+
+  //loop for player 1 to roll the dice
+ let player1Roll = prompt(name1 + ' type "roll" to roll the dice');
+ while(player1Roll != 'roll'){
+   player1Roll = prompt(name1 + ' type "roll" to roll the dice')
+ } console.log(name1 + ' got ' + roll1);
+
+//loop for player 2 to roll the dice
+ let player2Roll = prompt(name2 + ' type "roll" to roll the dice');
+ while(player2Roll != 'roll'){
+   player2Roll = prompt(name2 + ' type "roll" to roll the dice')
+ } console.log(name2 + ' got ' + roll2);
+
+//declare the winner and add points
+if(roll1 > roll2){
+  console.log(name1 + ' wins this round!');
+  pointPlayer1++
+} else if(roll1 < roll2){
+  console.log(name2 + ' wins this round!');
+  pointPlayer2++
+} else{
+  console.log('Its a draw!');
+}
 
 //make a winner for after rounds
 let winner = '';
@@ -106,9 +187,9 @@ if(pointPlayer1 > pointPlayer2){
 } else{
    winner = `${name1} ${name2}`;
 }
- 
+
 //state winner of the round
-if(round<roundPick){
+if(round<pick){
   let plural = pointPlayer1!=1?'s':'';
 console.log(`${name1} has ${pointPlayer1} point${plural}!`)
 let plural2 = pointPlayer2!=1?'s':'';
@@ -118,9 +199,4 @@ console.log(`${name2} has ${pointPlayer2} point${plural2}!`)
   console.log(name2 + ' got ' + pointPlayer2 + ' points!');
   console.log('Congratulations ' + winner + ', you won!');
 }
-
-round++
-}while(round == roundPick)
-
 }
-
